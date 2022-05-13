40000de9fb4a86ff223f4b3ba28987c2365d1d58
durveshramane26 <durveshramane6@gmail.com>
2022-05-13 10:54:37 +0530

    done

--- app.js
+++ app.js
@@ -2,22 +2,44 @@ var inputText = document.querySelector("#input-text");
 var outputText = document.querySelector("#output-text");
 var eventBtn = document.querySelector(".btn-primary");
 
-eventBtn.addEventListener("click", (e) => {
-    e.preventDefault();
+// eventBtn.addEventListener("click", (e) => {
+//     e.preventDefault();
 
-    var serverUrl = "https://api.funtranslations.com/translate/minion.json" + "?" + "text=" + inputText.value;
+//     var serverUrl = "https://api.funtranslations.com/translate/minion.json" + "?" + "text=" + inputText.value;
 
-    function errorHandler(error) {
-        alert("something wrong happen! please try again later.", error);
+//     function errorHandler(error) {
+//         alert("something wrong happen! please try again later.", error);
+//     }
+
+//     fetch(serverUrl)
+//         .then((res) => {
+//             return res.json();
+//     })
+//     .then((data) => {
+//         var translatedText = data.contents.translated;
+//         outputText.innerHTML = translatedText;
+//     })
+//     .catch(errorHandler);
+// });
+
+const serverurl = "https://api.funtranslations.com/translate/minion.json?text="
+
+function generateURL(text) {
+    return serverurl + text ;
+}
+
+function clickHandler () {
+    
+    if(inputText.value === undefined || inputText.value === "") {
+        window.alert('Empty input!! Please enter text to get its translation');
     }
 
-    fetch(serverUrl)
-        .then((res) => {
-            return res.json();
-    })
-    .then((data) => {
-        var translatedText = data.contents.translated;
-        outputText.innerHTML = translatedText;
-    })
-    .catch(errorHandler);
-});
+    else {
+        fetch(generateURL(inputText.value))
+        .then(response => response.json())
+        .then(json => {outputText.innerHTML = json.contents.translated});
+
+    }
+}
+
+eventBtn.addEventListener('click', clickHandler);
